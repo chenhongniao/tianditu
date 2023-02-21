ed7f88005aa01936fce6c91cd157d08fec5869e6
chenhongniao <542171639@qq.com>
2023-02-21 11:41:54 +0800

    修改2，2023-02-21-11-40

--- src/views/tianditu.vue
+++ src/views/tianditu.vue
@@ -5,16 +5,24 @@
 import Map from 'ol/Map'
 import View from 'ol/View'
 import TileLayer from 'ol/layer/Tile.js';
-import XYZ from 'ol/source/XYZ.js';
+import WMTS from 'ol/source/WMTS.js';
+import * as olProj from 'ol/proj';
+import * as olExtent from 'ol/extent';
+import Attribution from 'ol/control/Attribution.js';
+
 
 export default {
   name: 'tianditu',
   data(){
     return {
       map: null,
-      key: '205b344d16c0dc0544b4b610328cedae',
+      key: '37d614f39eb9dcfa72b2f1ab5aff22ff',
       vec: 'http://t0.tianditu.gov.cn/vec_c/wmts?tk=',
-      cva: 'http://t0.tianditu.gov.cn/cva_c/wmts?tk='
+      cva: 'http://t0.tianditu.gov.cn/cva_c/wmts?tk=',
+      projection: olProj.get('EPSG:4326'),
+      resolutions: new Array(14),
+      matrixIds: new Array(14)
+
     }
   },
   computed:{
@@ -23,26 +31,61 @@ export default {
       url_vec: this.vec + this.key,
       url_cva: this.cva + this.key
       }
-    }   
+    },
+    projectionExtent(){
+      return this.projection.getExtent()
+    },
+    size(){
+      return olExtent.getWidth(this.projectionExtent) / 256
+    },
+    attribution(){
+      return new Attribution({
+        html: '<strong>数据来源：天地图</strong>'
+      })
+    },
+    wmtsLayer_cva(){
+      return new TileLayer({
+        title: '天地图矢量图层',
+        opacity: 1,
+        source: new WMTS({
+          attributions: [this.attribution],
+          url: url_cva,
+          matrixSet: 'c',
+          format: 'png'
+
+        })
+      })
+    }
+  },
+  created(){
+    this.computeArray()
   },
   mounted(){
     this.createMap()
   },
   methods: {
+    computeArray(){
+      this.resolutions.forEach((v,i)=>{
+        v = this.size / Math.pow(2,i)
+      });
+      this.matrixIds.forEach((v,i)=>{
+        v = i
+      })
+    },
     createMap(){
-      return new Map({
+      new Map({
         target: this.$refs.map,
         layers: [
           new TileLayer({
             title: '天地图矢量图层',
-            source: new XYZ({
+            source: new WMTS({
               url: this.urlCompute.url_vec,
               wrapX: false
             })
           }),
           new TileLayer({
             title: '天地图矢量注记',
-            source: new XYZ({
+            source: new WMTS({
               url: this.urlCompute.url_cva,
               wrapX: false
             })
@@ -51,7 +94,7 @@ export default {
         view: new View({
           center: [ 0, 0],
           zoom: 3,
-          projection:"EPSG:4326"
+          projection: projection
         })
       })
     }
