61200bb934fe0e59102a1dc51e72a312852c62d5
chenhongniao <542171639@qq.com>
2023-02-28 13:56:14 +0800

    对项目文件进行完善，删除了一些无关的注释

--- src/views/tianditu.vue
+++ src/views/tianditu.vue
@@ -2,6 +2,7 @@
   <div class="map" ref="map"></div>
 </template>
 <script>
+// 引入模块
 import 'ol/ol.css'
 import Map from 'ol/Map'
 import View from 'ol/View'
@@ -15,6 +16,7 @@ import WMTSTileGrid from 'ol/tilegrid/WMTS';
 export default {
   name: 'tianditu',
   data() {
+    //定义变量
     return {
       map: null,
       key: '37d614f39eb9dcfa72b2f1ab5aff22ff',
@@ -23,72 +25,50 @@ export default {
       projection: olProj.get('EPSG:4326'),
       resolutions: new Array(19),
       matrixIds: new Array(19),
-      myLayer: []
+      layers: []
     }
   },
   computed: {
+    // 定义计算变量
     urlCompute() {
+      // 计算请求url
       return {
         url_vec: this.vec + this.key,
         url_cva: this.cva + this.key
       }
     },
     projectionExtent() {
+      // 计算投影范围
       return this.projection.getExtent()
     },
     size() {
+      // 
       return olExtent.getWidth(this.projectionExtent) / 256
     },
 
-    /* wmtsLayer_cva(){
-      return new TileLayer({
-        // title: '天地图矢量图层注记',
-        opacity: 1,
-        source: new WMTS({
-          attributions: '<i>数据来源：天地图</i>',
-          url: this.urlCompute.url_cva,
-          matrixSet: 'c',
-          format: 'png',
-          projection: this.projection,
-          tileGrid: new WMTSTileGrid({
-            origin: olExtent.getTopLeft(this.projectionExtent),
-            resolutions: this.resolutions,
-            matrixIds: this.matrixIds
-          }),
-          style: 'default',
-          wrapX: true
-        })
-      })
-    }, */
 
-    wmtsLayer_vec() { }
   },
   mounted() {
-    this.computeArray(this.resolutions, this.matrixIds),
-      // this.wmtsLayer(this.urlCompute.url_vec, this.projection, this.projectionExtent, this.resolutions, this.matrixIds),
-      // this.wmtsLayer(this.urlCompute.url_cva, this.projection, this.projectionExtent, this.resolutions, this.matrixIds),
-      this.wmtsLayer(this.urlCompute.url_vec),
-      this.wmtsLayer(this.urlCompute.url_cva),
-      this.createMap()
-    // this.test(this.attribution),
-    // this.test(this.urlCompute.url_cva),
-    // this.test(this.projection)
-    // this.test(this.projectionExtent),
-    // this.test(this.resolutions),
-    // this.test(this.matrixIds)
+    // 计算分辨率数组和矩阵标识列表
+    this.computeArray(this.resolutions, this.matrixIds)
+    // 创建WMTS图层对象
+    this.wmtsLayer(this.urlCompute.url_vec)
+    this.wmtsLayer(this.urlCompute.url_cva)
+    // 创建Map对象
+    this.createMap()
+
   },
   methods: {
     // 创建layers对象
     wmtsLayer(url, projection = this.projection, projectionExtent = this.projectionExtent, resolutions = this.resolutions, matrixIds = this.matrixIds) {
-      this.myLayer.push(
+      this.layers.push(
         new TileLayer({
-          // title: '天地图矢量图层注记',
           opacity: 1,
           source: new WMTS({
             attributions: '<i>数据来源：天地图</i>',
             url: url,
             matrixSet: 'c',
-            format: 'png',
+            format: 'tiles',
             projection: projection,
             tileGrid: new WMTSTileGrid({
               origin: olExtent.getTopLeft(projectionExtent),
@@ -101,8 +81,6 @@ export default {
         })
       )
     },
-
-
     computeArray(resolution, matrix) {
       // console.log(this);
       for (let i = 0; i < resolution.length; i++) {
@@ -111,11 +89,11 @@ export default {
       }
       // new Array() 创建的是一个稀疏数组，
       // 对于稀疏数组 map、filter、foreach 等方法并不会调用传入的第一个参数
-      /*       console.log(this);
+      /*    以下是错误示范
             this.resolutions = this.resolutions.map(function(_ ,i){
               console.log(this);
               return this.size / Math.pow(2,i)
-              // console.log(this);
+              
             });
              this.matrixIds= this.matrixIds.map(function(v,i){
               return i
@@ -124,7 +102,7 @@ export default {
     createMap() {
       this.map = new Map({
         target: this.$refs.map,
-        layers: this.myLayer,
+        layers: this.layers,
         view: new View({
           center: [0, 0],
           zoom: 3,
@@ -132,17 +110,12 @@ export default {
         })
       })
     },
-    test(val) {
-      console.log(val);
-    }
   }
-
-
 }
 </script>
 <style lang="less">
 .map {
   width: 100%;
-  height: 1000px;
+  height: 800px;
 }
 </style>
